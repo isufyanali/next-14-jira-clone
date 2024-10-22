aa47665fb07b9696a7e25117f0ad86a42cc0eb28
tom <wangtao19960223@gmail.com>
2024-10-22 15:37:19 +0800

    [add]image upload

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { z } from "zod";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
+import { ImageIcon } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Form,
@@ -25,7 +29,9 @@ interface CreateWorkspaceFormProps {
 }
 
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
-  const { mutate, isPending } = useCreateWorkspace()
+  const { mutate, isPending } = useCreateWorkspace();
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const form = useForm<z.infer<typeof createWorkspaceSchema>>({
     resolver: zodResolver(createWorkspaceSchema),
@@ -35,8 +41,24 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
   });
 
   const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
-    mutate({ json: values })
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : ""
+    }
+
+    mutate({ form: finalValues }, {
+      onSuccess: () => {
+        form.reset()
+      }
+    });
   };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if(file){
+      form.setValue("image", file)
+    }
+  }
 
   return (
     <Card className="w-full h-full border-none shadow-none">
@@ -65,7 +87,60 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                   </FormItem>
                 )}
               />
-              <DottedSeparator className="py-7"/>
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <div className="flex flex-col gap-y-2">
+                    <div className="flex items-center gap-x-5">
+                      {field.value ? (
+                        <div className="size-[72px] relative rounded-md overflow-hidden">
+                          <Image
+                            alt="logo"
+                            fill
+                            className="object-cover"
+                            src={
+                              field.value instanceof File
+                                ? URL.createObjectURL(field.value)
+                                : field.value
+                            }
+                          />
+                        </div>
+                      ) : (
+                        <Avatar className="size-[72px]">
+                          <AvatarFallback>
+                            <ImageIcon className="sixe-[36px] text-neutral-400" />
+                          </AvatarFallback>
+                        </Avatar>
+                      )}
+                      <div className="flex flex-col">
+                        <p className="text-sm">Workspace Icon</p>
+                        <p className="text-sm text-muted-foreground">
+                          JPG, PNG, SVG or JPEG, max 1mb
+                        </p>
+                        <input
+                          className="hidden"
+                          type="file"
+                          accept=".jpg, .png, .jpeg, .svg"
+                          ref={inputRef}
+                          onChange={handleImageChange}
+                          disabled={isPending}
+                        />
+                        <Button
+                          type="button"
+                          disabled={isPending}
+                          variant="teritary"
+                          className="w-fit mt-2"
+                          onClick={() => inputRef.current?.click()}
+                        >
+                          Upload Image
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              />
+              <DottedSeparator className="py-7" />
               <div className="flex items-center justify-between">
                 <Button
                   type="button"
@@ -76,11 +151,7 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                 >
                   Cancel
                 </Button>
-                <Button
-                  type="submit"
-                  size="lg"
-                  disabled={isPending}
-                >
+                <Button type="submit" size="lg" disabled={isPending}>
                   Create Workspace
                 </Button>
               </div>
--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -2,4 +2,13 @@ import { z } from "zod";
 
 export const createWorkspaceSchema = z.object({
   name: z.string().trim().min(1, "Required"),
+  image: z.union([
+    //這裡需要node > v20 才能執行
+    //在 Node.js 18 中，File 是一個在瀏覽器中存在的對象，並不是 Node.js 的內置對象
+    //因此，在 Node.js 18 中使用 z.instanceof(File) 會導致 File is not defined 錯誤
+    //要在 Node.js 中處理文件上傳，通常會使用類似 Buffer、Stream 或通過 multer 等中間件來處理文件，而不是直接使用 File。
+    z.instanceof(File),
+    z.string().transform((value) => value === "" ? undefined : value)
+  ])
+  .optional(),
 })
