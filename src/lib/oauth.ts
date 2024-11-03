24d053755958f767d6514eb37c3a9bb349ae72e6
tom <wangtao19960223@gmail.com>
2024-11-03 22:37:14 +0800

    [fix]fix oauth in prod

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -3,18 +3,15 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
 	const { account } = await createAdminClient();
-
-  const origin = headers().get("origin");
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Github,
-		`${origin}/oauth`,
-		`${origin}/sign-up`,
+		`${process.env.NEXT_PUBLIC_APP}/oauth`,
+		`${process.env.NEXT_PUBLIC_APP}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
@@ -22,13 +19,11 @@ export async function signUpWithGithub() {
 
 export async function signUpWithGoogle() {
 	const { account } = await createAdminClient();
-
-  const origin = headers().get("origin");
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${origin}/oauth`,
-		`${origin}/sign-up`,
+		`${process.env.NEXT_PUBLIC_APP}/oauth`,
+		`${process.env.NEXT_PUBLIC_APP}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
