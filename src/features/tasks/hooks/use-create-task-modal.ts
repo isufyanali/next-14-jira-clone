066dc339b225a0c416cfc48c77e29fa5a2cb29a1
tom <wangtao19960223@gmail.com>
2024-11-06 15:13:05 +0800

    [add]docker version

--- src/features/tasks/hooks/use-create-task-modal.ts
+++ src/features/tasks/hooks/use-create-task-modal.ts
@@ -1,6 +1,9 @@
 import { useQueryState, parseAsBoolean } from "nuqs"
 
 export const useCreateTaskModal = () => {
+  //如果要額外傳入別的參數判斷開啟的預設值,例如status
+  //使用useQueryStates
+
   const [isOpen, setIsOpen] = useQueryState(
     "create-task",
     parseAsBoolean.withDefault(false).withOptions({ clearOnDefault: true })
